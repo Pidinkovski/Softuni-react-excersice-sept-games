7538e2f828077aa0a305ca6b7b3c127e93c6a39b
Pidinkovski <pdinkovski@abv.bg>
2025-11-28 17:06:49 +0200

    create dynamic navigation user/guest

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -7,16 +7,17 @@ export default function Header({
             <nav>
                 <Link className="home" to="/"> <img src="./images/logo.png" alt="logo" /> </Link>
                 <Link to="/games">Catalog</Link>
-                {/* <!-- Logged-in users --> */}
-                <div id="user">
-                    <Link to="/create">Add Game</Link>
-                    <Link to="/logout">Logout</Link>
-                </div>
+
+                {user && 
+                    <div id="user">
+                        <Link to="/create">Add Game</Link>
+                        <Link to="/logout">Logout</Link>
+                    </div>}
                 {!user && 
-                <div id="guest">
-                    <Link to="/login">Login</Link>
-                    <Link to="/register">Register</Link>
-                </div>}
+                    <div id="guest">
+                        <Link to="/login">Login</Link>
+                        <Link to="/register">Register</Link>
+                    </div>}
                 
             </nav>
         </header>
